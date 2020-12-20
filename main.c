ec8a595ebb80455ddde896fb52c5dc6370986e21
MerHS <starvessel@naver.com>
2020-12-21 03:04:33 +0900

    add main

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include "src/sparch.h"
 
 int main() {
+  Matrix *a = Matrix_malloc(4, 4);
+  Matrix *b = Matrix_malloc(4, 4);
+
+  Matrix *gem = gemm_sparch(a, b);
+
+  Matrix_free(gem);
+  Matrix_free(b);
+  Matrix_free(a);
+  
   return 0;
 }
