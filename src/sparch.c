1dfa38ed58fbe4f9778538b4d0ae923aa587c912
MerHS <starvessel@naver.com>
2020-12-20 20:22:33 +0900

    unsigned to size_t / fix header

--- src/sparch.c
+++ src/sparch.c
@@ -1,4 +1,4 @@
-#include <stddef.h>
+#include <stdlib.h>
 #include "sparch.h"
 
 #define LOW_COMP 4
@@ -12,29 +12,45 @@ typedef struct _LLNode {
 } LLNode;
 
 typedef struct _COOChunk {
-    unsigned int len;
+    size_t len;
     LLNode* head;
     LLNode* tail;
 } COOChunk;
 
 typedef struct _PriorQ {
-    unsigned int count;
+    size_t count;
     COOChunk* heap[QUEUE_MAX];
 } PriorQ;
 
 // Tier 0. outer product
+LLNode* LLNode_malloc(COOItem *item);
+void LLNode_free(LLNode *node);
+void LLNode_freeAll(LLNode *node);
+
+void COOChunk_free(COOChunk *chunk);
+void COOChunk_freeAll(COOChunk *chunk);
+
+// calculate outer product of condensed column and matrix.
 COOChunk* outerProd(COOChunk *left, CSRMatrix* right);
 
+
 // Tier 1. hierarchical merger
+
+// 4x4 low merger
 COOChunk* mergeLow(COOChunk *left, COOChunk *right);
+
+// 4x4 top merger
 COOChunk* mergeTop(COOChunk *left, COOChunk *right);
+
+// 8-chunk zero eliminator
 void elimZero(COOChunk *chunk);
 
+
 // Tier 2. merge tree
-COOChunk* flattenByMergeTree(COOChunk** chunks, unsigned int len);
+COOChunk* flattenByMergeTree(COOChunk** chunks, size_t len);
 
 // Tier 3. matrix condensing
-COOChunk** condense(CSRMatrix* csr, unsigned int* outLen); 
+COOChunk** condense(CSRMatrix* csr, size_t* outLen); 
 
 // Tier 4. priority queue (to implement huffman tree scheduler)
 void addQueue(PriorQ *queue, COOChunk *chunk);
@@ -42,6 +58,36 @@ COOChunk* popQueue(PriorQ *queue, COOChunk *chunk);
 
 
 // == Function implementations ==
+LLNode* LLNode_malloc(COOItem *item) {    
+    LLNode *node = (LLNode *)malloc(sizeof(LLNode));
+    node->item = item;
+    node->next = NULL;
+    return node;
+}
+
+void LLNode_free(LLNode *node) {
+    free(node);
+}
+
+void LLNode_freeAll(LLNode *node) {
+    free(node->item);
+    free(node);
+}
+
+void COOChunk_free(COOChunk *chunk) {
+    free(chunk);
+}
+
+void COOChunk_freeAll(COOChunk *chunk) {
+    LLNode *head, *next;
+    head = chunk->head;
+    while (head) {
+        next = head->next;
+        LLNode_freeAll(head);
+        head = next;
+    }
+}
+
 COOChunk* outerProd(COOChunk *left, CSRMatrix* right) {
     return NULL;
 }
@@ -58,11 +104,11 @@ void elimZero(COOChunk *chunk) {
     return;
 }
 
-COOChunk* flattenByMergeTree(COOChunk** chunks, unsigned int len) {
+COOChunk* flattenByMergeTree(COOChunk** chunks, size_t len) {
     return NULL;
 }
 
-COOChunk** condense(CSRMatrix* csr, unsigned int* outLen) {
+COOChunk** condense(CSRMatrix* csr, size_t* outLen) {
     return NULL;
 } 
 
@@ -75,16 +121,64 @@ COOChunk* popQueue(PriorQ *queue, COOChunk *chunk) {
 }
 
 // Main implementations
+COOItem* COOItem_malloc(size_t row, size_t col, float value) {
+    COOItem *item = (COOItem *)malloc(sizeof(COOItem));
+    item->row = row;
+    item->col = col;
+    item->value = value;
+    return item;
+}
+
+void COOItem_free(COOItem* item) {
+    free(item);
+}
+
+Matrix* Matrix_malloc(size_t height, size_t width) {
+    Matrix *matrix;
+    float *values = (float *)malloc((height * width) * sizeof(float));
+    matrix = (Matrix *)malloc(sizeof(Matrix));
+
+    matrix->height = height;
+    matrix->width = width;
+    matrix->values = values;
+    
+    return matrix;
+}
+
+void Matrix_free(Matrix* matrix) {
+    free(matrix->values);
+    free(matrix);
+}
 
 CSRMatrix* Matrix_toCSR(Matrix* matrix) {
     return NULL;
 }
-COOMatrix* CSR_toCOO(CSRMatrix* csr) {
-    return NULL;
+
+CSRMatrix* CSR_malloc(size_t height, size_t width, size_t lenVal) {
+    CSRMatrix *csr;
+    size_t *cols, *rows;
+    float *values = (float *)malloc(lenVal * sizeof(float));
+    cols = (size_t*)malloc(lenVal * sizeof(size_t));
+    rows = (size_t*)malloc((height + 1) * sizeof(size_t));
+    csr = (CSRMatrix *)malloc(sizeof(CSRMatrix));
+
+    csr->height = height;
+    csr->width = width;
+    csr->lenVal = lenVal;
+    csr->values = values;
+    csr->cols = cols;
+    csr->rows = rows;
+
+    return csr;
 }
-CSRMatrix* COO_toCSR(COOMatrix* coo) {
-    return NULL;
+
+void CSR_free(CSRMatrix *csr) {
+    free(csr->values);
+    free(csr->cols);
+    free(csr->rows);
+    free(csr);
 }
+
 Matrix* CSR_dense(CSRMatrix* csr) {
     return NULL;
 }
@@ -95,10 +189,16 @@ CSRMatrix* spgemm_sparch(CSRMatrix* matA, CSRMatrix* matB) {
 
 Matrix* gemm_sparch(Matrix* matA, Matrix* matB) {
     CSRMatrix* left = Matrix_toCSR(matA);
-    CSRMatrix* right= Matrix_toCSR(matB);
-    CSRMatrix* result = spgemm_sparch(left, right);
+    CSRMatrix* right = Matrix_toCSR(matB);
+    CSRMatrix* mm = spgemm_sparch(left, right);
 
-    return CSR_dense(result);
+    Matrix* result = CSR_dense(mm);
+
+    CSR_free(mm);
+    CSR_free(right);
+    CSR_free(left);
+
+    return result;
 }
 
 
--- src/sparch.h
+++ src/sparch.h
@@ -1,42 +1,42 @@
 #ifndef __SPARCH_H_
 #define __SPARCH_H_
 
+#include <stddef.h>
+
 typedef struct _Matrix {
-    unsigned int height;
-    unsigned int width;
-    float** values;
+    size_t height;
+    size_t width;
+    float* values;
 } Matrix;
 
 typedef struct _COOItem {
-    unsigned int row;
-    unsigned int col;
+    size_t row;
+    size_t col;
     float value;
 } COOItem;
 
-typedef struct _COOMatrix {
-    unsigned int height;
-    unsigned int width;
-    unsigned int len;
-    COOItem* values; 
-} COOMatrix;
-
 typedef struct _CSRMatrix {
-    unsigned int height;
-    unsigned int width;
-    unsigned int lenVal;
-    unsigned int lenCol;
-    unsigned int lenRow;
+    size_t height;
+    size_t width;
+    size_t lenVal;
     float* values;
-    unsigned int* cols;
-    unsigned int* rows;
+    size_t* cols;
+    size_t* rows;
 } CSRMatrix;
 
+COOItem* COOItem_malloc(size_t row, size_t col, float value);
+void COOItem_free(COOItem* item);
+
+Matrix* Matrix_malloc(size_t height, size_t width);
+void Matrix_free(Matrix* matrix);
 CSRMatrix* Matrix_toCSR(Matrix* matrix);
-COOMatrix* CSR_toCOO(CSRMatrix* csr);
-CSRMatrix* COO_toCSR(COOMatrix* coo);
+
+CSRMatrix* CSR_malloc(size_t height, size_t width, size_t lenVal);
+void CSR_free(CSRMatrix *csr);
 Matrix* CSR_dense(CSRMatrix* csr);
 
 Matrix* gemm_sparch(Matrix* matA, Matrix* matB);
 CSRMatrix* spgemm_sparch(CSRMatrix* matA, CSRMatrix* matB);
+
 
 #endif // __SPARCH_H_
